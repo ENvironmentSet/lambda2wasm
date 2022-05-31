217c8d48a78e72152387a22a1274f9675e0627db
ENvironmentSet <sbshw1@gmail.com>
2022-05-31 23:32:50 +0900

    update syntax and parser

--- src/AST.ts
+++ src/AST.ts
@@ -8,7 +8,7 @@ export type LCAbsT = TaggedRecord<'LCAbsT', { param: LCType, ret: LCType }>;
 export type LCType = LCMVar | LCPVar | LCAbsT;
 
 export type LCVar = TaggedRecord<'LCVar', { id: string }>;
-export type LCNum = TaggedRecord<'LCNum', { literal: string }>;
+export type LCNum = TaggedRecord<'LCNum', { val: number, type: LCMVar | LCPVar }>;
 export type LCAbs = TaggedRecord<'LCAbs', { pID: string, pType: LCType, exp: LCExp }>;
 export type LCApp = TaggedRecord<'LCApp', { f: LCExp, arg: LCExp }>;
 export type LCAdd = TaggedRecord<'LCAdd', { left: LCExp, right: LCExp }>;
@@ -18,13 +18,17 @@ export type LCDiv = TaggedRecord<'LCDiv', { left: LCExp, right: LCExp }>;
 export type LCExp = LCVar | LCNum | LCAbs | LCApp | LCAdd | LCMult | LCSub | LCDiv;
 
 export type LCBind = TaggedRecord<'LCBind', { id: string, exp: LCExp }>;
+export type LCProc = TaggedRecord<'LCProc', { bindings: LCBind[] }>;
 
+export type AST = LCType | LCExp | LCBind | LCProc;
+
+//@serializable.
 export const LCMVar = (id: string): LCMVar => ({ tag: 'LCMVar', id });
 export const LCPVar = (id: string): LCPVar => ({ tag: 'LCPVar', id });
 export const LCAbsT = (param: LCType, ret: LCType): LCAbsT => ({ tag: 'LCAbsT', param, ret });
 
 export const LCVar = (id: string): LCVar => ({ tag: 'LCVar', id });
-export const LCNum = (literal: string): LCNum => ({ tag: 'LCNum', literal });
+export const LCNum = (val: number, type: LCMVar | LCPVar): LCNum => ({ tag: 'LCNum', val, type });
 export const LCAbs = (pID: string, pType: LCType, exp: LCExp): LCAbs => ({ tag: 'LCAbs', pID, pType, exp });
 export const LCApp = (f: LCExp, arg: LCExp): LCApp => ({ tag: 'LCApp', f, arg });
 export const LCAdd = (left: LCExp, right: LCExp): LCAdd => ({ tag: 'LCAdd', left, right });
@@ -33,3 +37,4 @@ export const LCSub = (left: LCExp, right: LCExp): LCSub => ({ tag: 'LCSub', left
 export const LCDiv = (left: LCExp, right: LCExp): LCDiv => ({ tag: 'LCDiv', left, right });
 
 export const LCBind = (id: string, exp: LCExp): LCBind => ({ tag: 'LCBind', id, exp });
+export const LCProc = (bindings: LCBind[]): LCProc => ({ tag: 'LCProc', bindings });
---
+++ src/refineSyntaxTree.ts
@@ -0,0 +1,83 @@
+import { abstraction, application_1, ASTKinds, binding, expression, function_type_1, start, type } from './parser';
+import {
+  LCAbs,
+  LCAbsT,
+  LCAdd,
+  LCApp,
+  LCBind,
+  LCDiv,
+  LCExp,
+  LCMult,
+  LCMVar,
+  LCNum,
+  LCProc,
+  LCPVar,
+  LCSub,
+  LCType,
+  LCVar
+} from './AST';
+
+function refineFunctionType(st: function_type_1): LCAbsT {
+  return LCAbsT(refineType(st.param), refineType(st.exp));
+}
+
+function refineType(st: type): LCType {
+  switch (st.kind) {
+    case ASTKinds.function_type_1:
+      return refineFunctionType(st)
+    case ASTKinds.type_2:
+      return refineType(st.type)
+    case ASTKinds.type_var_mono:
+      return LCMVar(st.identifier)
+    case ASTKinds.type_var_poly:
+      return LCPVar(st.identifier)
+    case ASTKinds.type_function_type_head_2:
+      return refineType(st.type)
+    default:
+      throw new Error(`non-exhaust switch statement in refineTy[e: ${st}`);
+  }
+}
+
+function refineAbstraction(st: abstraction): LCAbs {
+  return LCAbs(st.param.identifier, refineType(st.paramT), refineExp(st.body))
+}
+
+function refineApplication(st: application_1): LCApp {
+  return LCApp(refineExp(st.head), refineExp(st.arg.kind === ASTKinds.expression_argument_1 ? st.arg.exp : st.arg));
+}
+
+function refineExp(st: expression): LCExp {
+  switch (st.kind) {
+    case ASTKinds.expression_1:
+      return refineExp(st.exp)
+    case ASTKinds.additive_1:
+      return LCAdd(refineExp(st.left), refineExp(st.right))
+    case ASTKinds.additive_2:
+      return LCSub(refineExp(st.left), refineExp(st.right))
+    case ASTKinds.multiplicative_1:
+      return LCMult(refineExp(st.left), refineExp(st.right))
+    case ASTKinds.multiplicative_2:
+      return LCDiv(refineExp(st.left), refineExp(st.right))
+    case ASTKinds.abstraction:
+      return refineAbstraction(st)
+    case ASTKinds.application_1:
+      return refineApplication(st)
+    case ASTKinds.expression_head_1:
+      return refineExp(st.exp)
+    case ASTKinds.identifier:
+      return LCVar(st.identifier)
+    case ASTKinds.num:
+      return LCNum(st.value, st.type.kind === ASTKinds.type_var_poly ? LCPVar(st.type.identifier) : LCMVar(st.type.identifier))
+    default:
+      throw new Error(`non-exhaust switch statement in refineExp: ${st}`);
+  }
+}
+
+function refineBinding(st: binding): LCBind {
+  return LCBind(st.name.identifier, refineExp(st.exp));
+}
+
+export function refineSyntaxTree(st: start): LCProc {
+  return LCProc(st.bindings.map(refineBinding));
+}
+
