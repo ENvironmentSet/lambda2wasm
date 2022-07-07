8e97ecfa9731e1582ce25b5316fa05e303bc6393
ENvironmentSet <sbshw1@gmail.com>
2022-07-07 12:38:39 +0900

    polymorphic function supports

--- src/compiler.ts
+++ src/compiler.ts
@@ -19,12 +19,10 @@ function lcType2WasmType(lcType: LCType): string {
   if (lcType.tag === 'LCMVar') {
     return lcType.id.toLowerCase();
   } else if (lcType.tag === 'LCAbsT') return 'i64';
-  else throw new Error(`Cannot convert polymorphic type ${typeToString(lcType)} to wasm type. `);
+  else return 'v128';
 }
 
 function compileNumericLiteral(ast: LCNum): string {
-  if (ast.type.tag === 'LCPVar') throw new Error('Numeric Literal cannot have polymorphic type');
-
   switch (ast.type.id) {
     case 'I32':
       return `(i32.const ${ast.val})`;
@@ -79,9 +77,6 @@ function compileArithmetic(ast: LCArithmetic, context: Context): string {
 
 function compileAbstraction(ast: LCAbs, context: Context): string {
   const functionType = context.typeMap.get(ast)! as LCAbsT;
-
-  if (isPolyType(functionType)) throw new Error('Polymorphic Function is not implemented yet');
-
   const functionName = `${context.lambdaConfig.lambdaName}_${context.lambdaConfig.lambdaDepth}`;
   const paramType = lcType2WasmType(functionType.param);
   let contextInit = '';
@@ -118,14 +113,38 @@ function compileAbstraction(ast: LCAbs, context: Context): string {
 
 function compileApplication(ast: LCApp, context: Context): string {
   const signatureName = `$_sig_${context.lambdaConfig.lambdaName}_${context.sigMap.size}`;
-  const signature = `(type ${signatureName} (func (param ${lcType2WasmType((context.typeMap.get(ast.f) as LCAbsT).param)}) (param i32) (result ${lcType2WasmType((context.typeMap.get(ast.f) as LCAbsT).ret)})))`;
+  const paramType = (context.typeMap.get(ast.f) as LCAbsT).param;
+  const retType = (context.typeMap.get(ast.f) as LCAbsT).ret;
+  const expectedType = context.typeMap.get(ast)!;
+  const argType = context.typeMap.get(ast.arg)!;
+  const signature = `(type ${signatureName} (func (param ${lcType2WasmType(paramType)}) (param i32) (result ${lcType2WasmType(retType)})))`;
   //@FIXME: Monkey patch
 
   context.sigMap.set(signatureName, signature);
 
+  let argExp = compileExpression(ast.arg, context);
+
+  if (paramType.tag === 'LCPVar' && argType.tag !== 'LCPVar') {
+    const laneSize = /(i|f)32/.test(lcType2WasmType(argType)) ? `${lcType2WasmType(argType)}x4` : `${lcType2WasmType(argType)}x2`;
+    argExp = `
+      (${laneSize}.replace_lane 0 (v128.const i64x2 0 0) ${argExp})
+    `;
+  }
+
+  let callExp = `(call_indirect (type ${signatureName}) ${argExp} (call $get_context_from_closure (local.get $closure_ref_tmp)) (call $get_fref_from_closure (local.get $closure_ref_tmp)))`;
+
+  if (retType.tag === 'LCPVar' && expectedType.tag !== 'LCPVar') {
+    const laneSize = /(i|f)32/.test(lcType2WasmType(expectedType)) ? `${lcType2WasmType(expectedType)}x4` : `${lcType2WasmType(expectedType)}x2`;
+
+    callExp = `
+      ${callExp}
+      (${laneSize}.extract_lane 0)
+    `;
+  }
+
   return `
     (local.set $closure_ref_tmp ${compileExpression(ast.f, context)})
-    (call_indirect (type ${signatureName}) ${compileExpression(ast.arg, context)} (call $get_context_from_closure (local.get $closure_ref_tmp)) (call $get_fref_from_closure (local.get $closure_ref_tmp)) )
+    ${callExp}
   `;
 }
 
@@ -149,22 +168,14 @@ function compileExpression(ast: LCExp, context: Context): string {
   }
 }
 
-export function compile(ast: LCProc, typeMap: TypeMap): string {
+export function compile(ast: LCProc, typeMap: TypeMap, runtime: string): string {
   const functionMap = new Map<string, { code: string, tableIndex: number }>();
   let tableOffset = 0;
   const sigMap = new Map<string, string>();
+  //v128 cannot be passed between wasm runtime and js host.
   let result = `
     (module
-      (import "runtime" "create_context_i32" (func $create_context_i32 (param i32) (result i32)))
-      (import "runtime" "create_context_i64" (func $create_context_i64 (param i64) (result i32)))
-      (import "runtime" "alloc_context_var_i32" (func $alloc_context_var_i32 (param i32) (param i32) (result i32)))
-      (import "runtime" "alloc_context_var_i64" (func $alloc_context_var_i64 (param i64) (param i32) (result i32)))
-      (import "runtime" "read_i32_from_context" (func $read_i32_from_context (param i32) (param i32) (result i32)))
-      (import "runtime" "read_i64_from_context" (func $read_i64_from_context (param i32) (param i32) (result i64)))
-      (import "runtime" "create_closure" (func $create_closure (param i32) (param i32) (result i64)))
-      (import "runtime" "get_fref_from_closure" (func $get_fref_from_closure (param i64) (result i32)))
-      (import "runtime" "get_context_from_closure" (func $get_context_from_closure (param i64) (result i32)))
-
+    ${runtime}
   `;
 
   for (const binding of ast.bindings) {
